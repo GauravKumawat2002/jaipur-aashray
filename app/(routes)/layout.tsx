7501ccf0665539bd35e8f1ca67eeea10609409e7
Gaurav Kumawat <gauravkumawat830@gmail.com>
2025-03-25 07:58:09 +0530

    Update layout component: adjust padding for Navbar and Footer for improved responsiveness

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -9,11 +9,11 @@ export default function AppLayout({
 }>) {
   return (
     <>
-      <Navbar className="mx-auto max-w-[1440px] px-4 py-4 sm:px-6 lg:px-16 xl:px-32" />
-      <main className="mx-auto min-h-screen max-w-[1440px] px-4 py-4 sm:px-6 lg:px-16 xl:px-32">
+      <Navbar className="mx-auto max-w-[1440px] px-8 py-4 min-[1440px]:px-32" />
+      <main className="mx-auto min-h-screen max-w-[1440px] py-4 sm:px-6 lg:px-0 [1440px]:px-32">
         {children}
       </main>
-      <Footer className="mx-auto max-w-[1440px] px-4 py-4 sm:px-6 sm:py-6 lg:px-16 lg:py-20 xl:px-32" />
+      <Footer className="mx-auto max-w-[1440px] px-8 py-4 sm:py-6 lg:py-20 [1440px]:px-32" />
     </>
   );
 }
