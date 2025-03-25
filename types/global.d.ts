d721158bd7a24b3451aab154fb203bc1b4f8dd5d
Gaurav Kumawat <gauravkumawat830@gmail.com>
2025-03-25 07:50:38 +0530

    Add global TypeScript definitions for various component props and interfaces

---
+++ types/global.d.ts
@@ -0,0 +1,52 @@
+import { StaticImageData } from "next/image";
+
+export declare global {
+  interface StatItem {
+    number: string;
+    label: string;
+  }
+
+  interface CardProps {
+    title: string;
+    text: string;
+    buttonText: string;
+    ctaButtonLink: string;
+  }
+
+  interface AboutUsProps {
+    title: string;
+    subtitle: string;
+    tagline: string;
+    card: CardProps;
+    stats: StatItem[];
+    imageAlt: string;
+    className?: string;
+  }
+  interface PropertyCardDetails {
+    propertyImage: string | StaticImageData;
+    propertyName: string;
+    propertyImageAlt: string;
+    propertyLocation: string;
+    propertyPrice: string;
+    bhkDetails: {
+      bed: number;
+      hall: number;
+      kitchen: number;
+    };
+  }
+  interface PropertyCardProps {
+    className?: string;
+    cardDetails: PropertyCardDetails;
+  }
+  interface SectionTitleProps {
+    className?: string;
+    title: string;
+    primaryColor?: string;
+  }
+  interface PropertySectionProps {
+    sectionTitle: string;
+    viewMoreButtonText?: string;
+    properties: PropertyCardDetails[];
+    className?: string;
+  }
+}
