f765e5092021c723abe260beff9bc373de318e59
Gaurav Kumawat <gauravkumawat830@gmail.com>
2025-03-25 07:52:24 +0530

    Add SectionTitle component for enhanced title styling

---
+++ components/custom/shared/section-title.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+
+export default function SectionTitle({
+  primaryColor = "#c22126",
+  title,
+  className,
+}: SectionTitleProps) {
+  return (
+    <h1
+      className={cn("text-4xl leading-tight font-bold md:text-5xl", className)}
+    >
+      {title.split(" ").slice(0, -1).join(" ")}{" "}
+      <span style={{ color: primaryColor }} className="text-5xl md:text-6xl">
+        {title.split(" ").slice(-1)}
+      </span>
+    </h1>
+  );
+}
