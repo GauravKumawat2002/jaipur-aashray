5f1f5f49c9b14e80a9b16195fd804211c7c447c2
Gaurav Kumawat <gauravkumawat830@gmail.com>
2025-03-25 07:54:36 +0530

    Add StatsItem component for displaying a number with a label using a slot counter

---
+++ components/custom/home/stats-item.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SlotCounter from "react-slot-counter";
+const StatsItem: React.FC<StatItem> = ({ number, label }) => {
+  return (
+    <div className="flex flex-col">
+      <SlotCounter
+        value={number}
+        charClassName="text-red-600 font-bold text-3xl"
+      />
+      <span className="text-gray-600">{label}</span>
+    </div>
+  );
+};
+
+export default StatsItem;
